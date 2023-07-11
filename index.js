12c3362800d5d110e614899357944fff932e7da8
Mukhammadali <ali_6017@mail.ru>
2023-07-11 23:17:58 +0500

    https request

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const fs = require("fs")
-// console.log(fs);
+const https = require("https")
 
-fs.mkdir("./test", {}, (error) => {
-  console.log(error);
-  if (!error) {
-    console.log("Papka yaratildi..");
-  }
+https.get('https://jsonplaceholder.typicode.com/todos/1', (response) => {
+  response.setEncoding('utf-8')
+
+  let data = "";
+  response.on('data', (chunk) => {
+    data = data + chunk;
+  })
+
+  response.on('end', () => {
+    try {
+      let obj = JSON.parse(data)
+      console.log(obj);
+    } catch (error) {
+      console.log(error.message);
+    }
+  })
 })
