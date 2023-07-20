fde2eb6365660dae5aac117e74fb7c6d591a16ab
Mukhammadali <ali_6017@mail.ru>
2023-07-20 19:40:36 +0500

    Example Promise, async function

--- index.js
+++ index.js
@@ -1,19 +1,42 @@
-const https = require("https")
+const {
+  rejects
+} = require("assert");
+const https = require("https");
+const {
+  resolve
+} = require("path");
 
-https.get('https://api.telegram.org/bot6377274441:AAHoE6irgDgvd3jxqqYZxCACEtsFGDa6I1w/getMe', (response) => {
-  response.setEncoding('utf-8')
+function getUpdates() {
+  https.get('https://api.telegram.org/bot6377274441:AAHoE6irgDgvd3jxqqYZxCACEtsFGDa6I1w/getUpdates', (response) => {
+    response.setEncoding('utf-8')
 
-  let data = "";
-  response.on('data', (chunk) => {
-    data = data + chunk;
+    let data = "";
+    response.on('data', (chunk) => {
+      data = data + chunk;
+    })
+
+    response.on('end', () => {
+      try {
+        let obj = JSON.parse(data)
+        console.log(obj);
+      } catch (error) {
+        console.log(error.message);
+      }
+    })
   })
+}
+getUpdates()
 
-  response.on('end', () => {
-    try {
-      let obj = JSON.parse(data)
-      console.log(obj);
-    } catch (error) {
-      console.log(error.message);
-    }
-  })
+// Promise
+let m = new Promise((resolve, rejects) => {
+  setTimeout(() => {
+    resolve(1982)
+  }, 3000)
 })
+
+async function main() {
+  let a = await m;
+  console.log(a);
+}
+
+main()
