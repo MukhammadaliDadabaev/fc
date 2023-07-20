31225de0e483cc4e4e3ce57eff555530017451d1
Mukhammadali <ali_6017@mail.ru>
2023-07-20 22:24:29 +0500

    sendMessage-methods

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-import axios from "axios";
+import axios, {
+  formToJSON
+} from "axios";
 
 async function TelegramBot() {
   let updates = await axios.get('https://api.telegram.org/bot6377274441:AAHoE6irgDgvd3jxqqYZxCACEtsFGDa6I1w/getUpdates')
 
-  // if (!updates ? data ? ok) return;
+  for (let message of updates.data.result) {
+    sendRequest("sendMessage", {
+      chat_id: message.message.chat.id,
+      // text: "<b>ASSALAMU ALAYKUM</b>",
+      text: "*bold \*ASSALAMU ALAYKUM*",
+      disable_notification: true,
+      reply_to_message_id: message.message.message_id,
+      parse_mode: "html"
+    })
+  }
 
-  updates.data.result.forEach(element => {
-    console.log(element);
-  });
 }
 
 TelegramBot()
+
+async function sendRequest(methodName, options) {
+  let response = await axios.post("https://api.telegram.org/bot6377274441:AAHoE6irgDgvd3jxqqYZxCACEtsFGDa6I1w/" + methodName, options)
+
+  return response.data;
+}
