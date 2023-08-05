0924c25f37bcc480ed5d3e64e5cf47c128ee3a65
Mukhammadali Dadabaev <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-08-05 16:15:33 +0500

    practice test bot

--- index.js
+++ index.js
@@ -5,32 +5,20 @@ const bot = new TelegramBot("6377274441:AAHoE6irgDgvd3jxqqYZxCACEtsFGDa6I1w", {
 });
 
 bot.on("message", (updated) => {
-  bot.sendMessage(updated.chat.id, "ASSALAMU ALAYKUM", {
-    reply_markup: {
-      inline_keyboard: [
-        [
-          {
-            text: "Uni-1 bos",
-            callback_data: "Meni bos",
-          },
-          {
-            text: "Link-1 bos",
-            url: "https://github.com",
-          },
-        ], // BU BIRINCHI-QATOR
-        [
-          {
-            text: "Uni-2 bos",
-            callback_data: "Meni bos",
-          },
-          {
-            text: "Link-2 bos",
-            url: "https://github.com",
-          },
-        ], // BU IKKINCHI-QATOR
-      ],
-    },
-  });
+  if (updated.text === "/start") {
+    bot.sendMessage(updated.chat.id, "Menyuni tanglang 👇", {
+      reply_markup: {
+        keyboard: [["Narhlar"], ["Aloqa"]],
+        resize_keyboard: true,
+      },
+    });
+  } else if (updated.text === "Aloqa") {
+    bot.sendMessage(updated.chat.id, "Biz bilan boglanish +998897101010");
+  } else if (updated.text === "Narhlar") {
+    bot.sendMessage(updated.chat.id, "Barchasi 8880000 sum");
+  } else if (updated.text === "Okey") {
+    bot.sendMessage(updated.chat.id, "Rahmat 😎");
+  }
 });
 
 // 1- remove_keyboard: true,  => OLIB-TASHLAYDI
