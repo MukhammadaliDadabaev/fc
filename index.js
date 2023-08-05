ef2835d005a529814b3a4516d27014c9f7723b1d
Mukhammadali Dadabaev <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-08-05 15:11:12 +0500

    example keyboard 3-usul

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ const keyboard = [key_1, key_2, key_3];
 bot.on("message", (updated) => {
   bot.sendMessage(updated.chat.id, "ASSALAMU ALAYKUM", {
     reply_markup: {
-      keyboard: keyboard,
+      // remove_keyboard: true,
+      one_time_keyboard: true,
+      keyboard,
+      // resize_keyboard: true,
     },
   });
 });
+
+// 1- remove_keyboard: true,  => OLIB-TASHLAYDI
+// 2- one_time_keyboard: true => BIRMARTA ISHLAYDI
+// 3- resize_keyboard: true   => IHCHAM QILADI TELEFONGA-MOSLAYDI
