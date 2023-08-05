764e957fe07643cd1d499130f0e8f09ba31cd83e
Mukhammadali Dadabaev <92136722+MukhammadaliDadabaev@users.noreply.github.com>
2023-08-05 15:02:46 +0500

    example keyboard 2-usul

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ const bot = new TelegramBot("6377274441:AAHoE6irgDgvd3jxqqYZxCACEtsFGDa6I1w", {
   polling: true,
 });
 
+const key_1 = ["TEST-1", "TEST-2", "TEST-3"];
+const key_2 = ["TEST-1", "TEST-2", "TEST-3"];
+const key_3 = ["TEST-1", "TEST-2", "TEST-3"];
+const keyboard = [key_1, key_2, key_3];
+
 bot.on("message", (updated) => {
   bot.sendMessage(updated.chat.id, "ASSALAMU ALAYKUM", {
     reply_markup: {
-      keyboard: [["TEST-1"], ["TEST-2"], ["TEST-3"]],
+      keyboard: keyboard,
     },
   });
 });
